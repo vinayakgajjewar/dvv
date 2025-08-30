edaf5b05a2c4c80fdbf5492777d3dd0d3eb220fa
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2025-08-29 20:16:36 -0700

    made progress on helper funcs

--- include/dvv.hpp
+++ include/dvv.hpp
@@ -1,16 +1,32 @@
 #ifndef INCLUDE_DVV_HPP
 #define INCLUDE_DVV_HPP
 
-namespace dvv {
-    typedef struct {
-        std::string value;
-        std::unordered_map<std::string, int> vv;
-        std::tuple<std::string, int> dot;
-    } dvv;
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
-    void sync();
+using dvv = std::unordered_map<std::string, std::vector<int>>;
 
-    void update();
-}
+std::vector<std::string> dvv_ids(const dvv &);
+
+std::vector<std::string> dvv_ids(const std::vector<dvv> &);
+
+bool elem_leq(const std::vector<int> &, const std::vector<int> &);
+
+bool dvv_descends(const std::vector<dvv> &, const std::vector<dvv> &);
+
+bool dvv_dominates(const std::vector<dvv> &, const std::vector<dvv> &);
+
+/*
+ * TODO
+ *
+ * Everything below this comment is the public facing interface. How do I hide
+ * everything else?
+ */
+
+std::vector<dvv> dvv_sync(const std::vector<dvv> &, const std::vector<dvv> &);
+
+std::vector<dvv> dvv_update(const std::vector<dvv> &, const std::vector<dvv> &, std::string);
 
 #endif
--- src/dvv.cpp
+++ src/dvv.cpp
@@ -1,5 +1,41 @@
 #include "../include/dvv.hpp"
 
-void dvv::sync() {}
+std::vector<std::string> dvv_ids(const dvv &a) {
+    std::vector<std::string> ids;
+    for (const auto &[id, counters]: a) {
+        ids.push_back(id);
+    }
+    return ids;
+}
 
-void dvv::update() {}
+std::vector<std::string> dvv_ids(const std::vector<dvv> &v) {
+    std::vector<std::string> ids;
+    for (const auto &d: v) {
+        auto d_ids = dvv_ids(d);
+        ids.insert(ids.end(), d_ids.begin(), d_ids.end());
+    }
+    return ids;
+}
+
+bool elem_leq(const std::vector<int> &a, const std::vector<int> &b) {
+    auto asz = a.size();
+    auto bsz = b.size();
+    assert(asz <= 2);
+    assert(bsz <= 2);
+    if (asz == 1 && bsz == 1) return (a.at(0) <= b.at(0));
+    if (asz == 1) return (a.at(0) <= b.at(0) || (b.at(0) == b.at(1) + 1 && a.at(0) == b.at(1)));
+    if (bsz == 1) return (a.at(1) <= b.at(0));
+    return (a.at(1) <= b.at(0) || (a.at(0) <= b.at(0) && a.at(1) == b.at(1)));
+}
+
+bool dvv_descends(const std::vector<dvv> &a, const std::vector<dvv> &b) {
+    // TODO
+}
+
+bool dvv_dominates(const std::vector<dvv> &a, const std::vector<dvv> &b);
+
+std::vector<dvv> dvv_sync(const std::vector<dvv> &a, const std::vector<dvv> &b) {
+
+}
+
+std::vector<dvv> dvv_update() {}
